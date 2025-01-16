aa46ebfe6352c26190ef201f0c7426a91d9108d0
WitBroodje <jessyluiten14@gmail.com>
2025-01-16 13:06:11 +0100

    yes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuButton = document.querySelector("button[type='button']");
+  const mobileMenu = document.getElementById("mobile-menu");
 
+  menuButton.addEventListener("click", function () {
+    // Toggle the `hidden` class to show/hide the menu
+    mobileMenu.classList.toggle("hidden");
+  });
+});
